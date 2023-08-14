a7eaec247272322879b58415e01a1cb80882c439
Khaled <khaled.ali.architect@gmail>
2023-08-14 16:46:30 +0300

    Project Resetup
    See: #9

---
+++ backend/src/main/java/com/davi/shop/dto/ProductDTO.java
@@ -0,0 +1,131 @@
+package com.davi.shop.dto;
+
+import com.davi.shop.entities.Product;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ProductDTO {
+    private String name;
+    private String sku;
+    private String description;
+
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private Boolean active;
+    private Integer unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdated;
+    private ProductCategoryDTO category;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(ProductCategoryDTO category, String sku, String name, String description, BigDecimal unitPrice, String imageUrl, Boolean active, Integer unitsInStock, Date dateCreated, Date lastUpdated) {
+        this.category = category;
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.imageUrl = imageUrl;
+        this.active = active;
+        this.unitsInStock = unitsInStock;
+        this.dateCreated = dateCreated;
+        this.lastUpdated = lastUpdated;
+    }
+
+    public ProductDTO(Product entity) {
+        category = new ProductCategoryDTO(entity.getCategory());
+        sku = entity.getSku();
+        name = entity.getName();
+        description = entity.getDescription();
+        unitPrice = entity.getUnitPrice();
+        imageUrl = entity.getImageUrl();
+        active = entity.getActive();
+        unitsInStock = entity.getUnitsInStock();
+        dateCreated = entity.getDateCreated();
+        lastUpdated = entity.getLastUpdated();
+    }
+
+    public ProductCategoryDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategoryDTO category) {
+        this.category = category;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Integer getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(Integer unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
